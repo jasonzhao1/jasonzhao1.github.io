fbeec149fd1246b19dc8302488c38737961464bb
Jason Zhao <jasonzhao@Jasons-MacBook-Pro.local>
2020-09-13 16:59:16 -0400

    Updates

---
+++ precache-manifest.b312efafb75c764f18d5b39935be66dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "970668ebc9947387ab0bec6207d9a567",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67a6b61e3c815ed25f6c",
+    "url": "/static/js/2.180d86fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.180d86fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c6ab2b77907c76524c1",
+    "url": "/static/js/main.f7c7228a.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
